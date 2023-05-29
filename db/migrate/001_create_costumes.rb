bd609b1920959760f3a978b5043efca965b94f24
mark mwendwa <mwendwamark310@gmail.com>
2023-05-29 18:25:03 +0300

    completed assignment

---
+++ db/migrate/001_create_costumes.rb
@@ -0,0 +1,12 @@
+ # CreateCostumes migration file
+class CreateCostumes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps
+    end
+  end
+end
